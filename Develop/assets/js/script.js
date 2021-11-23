7b28e8c9ac795267efebc66011a98906ac64ef52
Mariana Gonzalez <mariana.gonzalez@wizeline.com>
2021-11-22 21:23:33 -0600

    refactor algorithm of generatePassword

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,64 +1,4 @@
 // Assignment Code
-const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const SYMBOLS = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  ",",
-  "*",
-  "+",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "]",
-  "^",
-  "_",
-  "{",
-  "|",
-  "}",
-  "~",
-];
-const LETTERS = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
 const MIN_LENGTH_PASSWORD = 8;
 const MAX_LENGTH_PASSWORD = 128;
 let passwordLength = 0;
@@ -118,42 +58,30 @@ function setPasswordConfiguration() {
   passwordLength = setPasswordLength();
   return (password = generatePassword(passwordFlags));
 }
-
 /**
- * This function will return a random character in the Array given.
- */
-function randomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-/**
- * Declaration of the characters that will contain the password like: numbers, special characters and letters.
- * Also I will add the min and max length for the password.
  * This function will create the password. The password should have at least:
  * At least one special character,
  * At least number
  * At least one upper case letter.
  */
 function generatePassword({ hasUppercaseChars, hasNumbers, hasSymbols }) {
-  const PASSWORD = [];
-  for (let i = 0; i < passwordLength; i++) {
-    //If the index is divisible by 3 we are going to  upper case the letter and added to the password and continue to the next iteration
-    if (hasUppercaseChars && i % 3 == 0) {
-      PASSWORD.push(randomElement(LETTERS).toUpperCase());
-      continue;
-    }
-    //If the index number is divisible by 4 we are going to add special characters to the password and continue to the next iteration
-    if (hasSymbols && i % 4 == 0) {
-      PASSWORD.push(randomElement(SYMBOLS));
-      continue;
-    }
-    //If the index number is divisible by 2 we are going to add a number to the password and continue to the next iteration
-    if (hasNumbers && i % 2 == 0) {
-      PASSWORD.push(randomElement(NUMBERS));
-      continue;
-    }
-    PASSWORD.push(randomElement(LETTERS));
+  let password = "abcdefghijklmnopqrstuvwxyz";
+  let passwordResult = "";
+  if (hasUppercaseChars) {
+    password += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  return PASSWORD.join("");
+  if (hasNumbers) {
+    password += "0123456789";
+  }
+  if (hasSymbols) {
+    password += "!@#$%^&*()_-+=|}{[];:?/><,.~";
+  }
+  for (let i = 0; i < passwordLength; i++) {
+    passwordResult += password.charAt(
+      Math.floor(Math.random() * password.length)
+    );
+  }
+  return passwordResult;
 }
 
 // Add event listener to generate button
